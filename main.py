dcad6b99ebc51e2d7a82630e179868ac90a69a98
CasperBerg <casperberg@msn.com>
2018-11-30 23:50:27 +0100

    todo

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ class Chess:
         self.user_move_from = None
         self.user_move_to = None
         self.move = False
-        self.player = False # False is player 1(white) and True is player 2(black)
+        self.player = True # False is player 1(white) and True is player 2(black)
 
         self.white_pieces = [u"\u2654", u"\u2655", u"\u2656",u"\u2657",u"\u2658",u"\u2659"]
         self.black_pieces = [u"\u265A", u"\u265B", u"\u265C",u"\u265D",u"\u265E",u"\u265F"]
-        self.layoutLenght = len(self.layout)
 
         self.move_king = None
         self.move_rock = None
@@ -52,6 +51,32 @@ class Chess:
       for item, string in enumerate(object):
         if index == int(self.user_move_from[0:1]) or item == int(self.user_move_from[1:2]):
           self.rank.append(str(index)+str(item))
+
+    self.unmovable = []
+    for indx, xy in enumerate(self.rank):
+      if self.layout[int(xy[0])][int(xy[1])] in self.white_pieces \
+        or self.layout[int(xy[0])][int(xy[1])] in self.black_pieces:
+        self.unmovable.append(xy)
+
+    for xy in self.unmovable:
+      self.rank.remove(xy)
+
+    x=0;y=0
+    for index, object in enumerate(self.layout):
+      x+=1
+      for item, string in enumerate(object):
+        y+=1; break
+
+    for _y in range(int(self.user.move_from[0:1]), y):
+      print("_Y",_y)
+
+
+    for _y in reversed(range(int(self.user_move_from[0:1]), 0):
+        pass
+        #print("_Y",_y)
+                                                      ######### TOOO DOOO 
+
+    print("rank"+str(self.rank))
     return self.rank
 
   def moveRules(self):
@@ -75,11 +100,24 @@ class Chess:
         self.queen_bishop_move.append(str(int(i[0])+int(self.loop))+str(int(self.user_move_from[1:2])-self.loop))
         self.queen_bishop_move.append(str(int(i[0])-int(self.loop))+str(int(self.user_move_from[1:2])+self.loop))
         self.queen_bishop_move.append(str(int(i[0])+int(self.loop))+str(int(self.user_move_from[1:2])+self.loop))
-    print(self.queen_bishop_move)
+
+    # Knight rules
+    self.knight = [
+        str(int(self.user_move_from[0:1])+1)+str(int(self.user_move_from[1:2])+2),
+        str(int(self.user_move_from[0:1])-1)+str(int(self.user_move_from[1:2])-2),
+        str(int(self.user_move_from[0:1])+2)+str(int(self.user_move_from[1:2])+1),
+        str(int(self.user_move_from[0:1])-2)+str(int(self.user_move_from[1:2])-1),
+
+        str(int(self.user_move_from[0:1])+2)+str(int(self.user_move_from[1:2])-1),
+        str(int(self.user_move_from[0:1])-2)+str(int(self.user_move_from[1:2])+1),
+        str(int(self.user_move_from[0:1])+1)+str(int(self.user_move_from[1:2])-2),
+        str(int(self.user_move_from[0:1])-1)+str(int(self.user_move_from[1:2])+2)
+    ]
 
     # Pawn rules
     self.white_pawn = [int(self.user_move_from[0:1])-1, int(self.user_move_from[0:1])-2]
     self.black_pawn = [int(self.user_move_from[0:1])+1, int(self.user_move_from[0:1])+2]
+
 
     # King move
     if self.move_king == True:
@@ -110,9 +148,14 @@ class Chess:
         self.movement()
         self.move_bishop = False
 
+    # Knight move
+    elif self.move_knight == True:
+      if self.user_move_to in self.knight:
+        self.movement()
+        self.move_knight = False
+
     # Pawn move
     elif self.move_pawn == True:
-      print(self.white_pawn_boost)
       if self.player == False \
         and int(self.user_move_to[0:1]) in self.white_pawn \
         and self.user_move_to[1:2] == self.user_move_from[1:2] \
@@ -165,6 +208,11 @@ class Chess:
           self.move_bishop = True
           self.moveRules()
 
+        # Knight
+        elif self.piece == self.white_pieces[4] or self.piece == self.black_pieces[4]:
+          self.move_knight = True
+          self.moveRules()
+
         # Pawn
         elif self.piece == self.white_pieces[5] or self.piece == self.black_pieces[5]:
           self.move_pawn = True
@@ -195,8 +243,8 @@ class Chess:
         self.pieceMove()
       self.outlines()
       self.userinput()
-      #if self.player == False: self.player = True
-      #else: self.player = False
+      if self.player == False: self.player = True
+      else: self.player = False
       self.move = True
 
   def run(self):
